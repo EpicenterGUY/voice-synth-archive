36491ffa1e3efe92d47dd816cf1cca598a123e6c
EpicenterGUY <mintbamtol7@gmail.com>
2026-09-25 10:47:17 +0900

    Bump service worker to v21.0.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION="21.0.0";
+const SW_VERSION="21.0.1";
 const CACHE_NAME="voice-synth-archive-shell-"+SW_VERSION;
 const SHELL=[
   "./",
@@ -13,7 +13,7 @@ const SHELL=[
   "./detective-v14.js?v=14.0",
   "./detective-v16.js?v=19.0",
   "./discovery-v20.js?v=20.0",
-  "./pwa-v21.js?v=21.0.0"
+  "./pwa-v21.js?v=21.0.1"
 ];
 
 self.addEventListener("install",event=>{
