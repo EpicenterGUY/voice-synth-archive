84f8a08b3488f686b92ef8f8bcfb3acf04d75fec
EpicenterGUY <mintbamtol7@gmail.com>
2026-09-25 12:11:22 +0900

    Bump app version to v28.1.0

--- pwa-v21.js
+++ pwa-v21.js
@@ -1,7 +1,7 @@
 /* Voice Synth Archive PWA v21 */
 (function(){
 "use strict";
-const APP_VERSION="28.0.0";
+const APP_VERSION="28.1.0";
 const CHECK_MS=60000;
 let deferredInstall=null;
 let registration=null;
