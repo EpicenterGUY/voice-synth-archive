c7d7896f2b714683df9421540b747b39af65e993
EpicenterGUY <mintbamtol7@gmail.com>
2026-09-25 18:44:58 +0900

    Bump PWA helper to v37.9.0

--- pwa-v21.js
+++ pwa-v21.js
@@ -1,7 +1,7 @@
 /* Voice Synth Archive PWA v21 */
 (function(){
 "use strict";
-const APP_VERSION="37.8.0";
+const APP_VERSION="37.9.0";
 const CHECK_MS=60000;
 let deferredInstall=null;
 let registration=null;
